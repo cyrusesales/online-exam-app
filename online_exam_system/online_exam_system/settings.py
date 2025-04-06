d98b74093f92cc36482c6bc34f3e9d3c969307fa
Cyrus Sales <cyrus.sales@outlook.com>
2025-04-06 16:39:19 +0800

    added code in add_category.html

--- online_exam_system/online_exam_system/settings.py
+++ online_exam_system/online_exam_system/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'online_exam_db',
         'USER': 'root',
-        'PASSWORD': 'password',
+        'PASSWORD': 'Password@123',
         'HOST': 'localhost',
         'PORT': '3306',
     }
